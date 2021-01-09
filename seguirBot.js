dbfa3c270ab80734e89cef87cf612d095f288b7d
Kaique Barreto <51871012+kaiquebarreto@users.noreply.github.com>
2021-01-09 01:19:43 -0300

    Versão 1.1 do Bot Finalizada

--- seguirBot.js
+++ seguirBot.js
@@ -1,7 +1,12 @@
+const quantidadeSeguir = 30; // Vai seguir 30 pessoas
+let segundosAtraso =  5; // 5 Segundos de Atraso 
+let divisaoAtraso = segundosAtraso / 100;
+let atraso = divisaoAtraso;
+
 (() => {
     let i = 0;
     const intervaloparaSeguir = setInterval(() => {
-        if (i >= 30) {
+        if (i >= quantidadeSeguir) {
             clearInterval(intervaloparaSeguir);
             return;
         }
@@ -9,5 +14,5 @@
         const proximoBotao = botoes[i];
         proximoBotao.click();
         i += 1;
-    }, 500)
+    }, atraso)
 })()
